b19ca28662dc48c1f44a503dcd0906adfdf64a6f
Dr. Necrotix (NIKO) <nikolastoyanov96@gmail.com>
2026-09-08 21:53:55 +0300

    fix: providers-core re-exports a+b modules

--- src/modules/digital-footprint/providers-core.ts
+++ src/modules/digital-footprint/providers-core.ts
@@ -1,1 +1,5 @@
-CORE_CONTENT_FROM_ARTIFACT
+export {
+    finding, pickExposed, asExposed, safeSourceUrl,
+    hibp, holehe, emailRep, gravatar, github, gitlab, domain,
+} from './providers-core-a';
+export { leakCheckPublic, xposedOrNot, hudsonRock } from './providers-core-b';
