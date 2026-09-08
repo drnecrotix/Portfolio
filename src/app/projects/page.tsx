a2f336045896faddd8a119ea3daf7c6e23427408
Dr. Necrotix (NIKO) <nikolastoyanov96@gmail.com>
2026-09-08 09:42:52 +0300

    security: protect managed media in src/app/projects/page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,7 @@
 import { prisma } from '@/lib/prisma';
 import { cmsProjectToPortfolioProject } from '@/lib/cms-projects';
 import { ProjectsArchiveClient } from '@/components/projects/ProjectsArchiveClient';
+import { protectManagedMediaUrls } from '@/lib/blog-media-protection';
 
 export const dynamic = 'force-dynamic';
 
@@ -12,5 +13,13 @@ export default async function ProjectsPage() {
         orderBy: [{ sortOrder: 'asc' }, { createdAt: 'desc' }],
     });
 
-    return <ProjectsArchiveClient projects={cmsProjects.map(cmsProjectToPortfolioProject)} />;
+    const projects = cmsProjects.map(cmsProjectToPortfolioProject);
+    const replacements = await protectManagedMediaUrls(projects.flatMap((project) => [project.image ?? '', ...(project.galleryImages ?? [])]));
+    const protectedProjects = projects.map((project) => ({
+        ...project,
+        image: project.image ? replacements.get(project.image) ?? project.image : project.image,
+        galleryImages: project.galleryImages?.map((image) => replacements.get(image) ?? image),
+    }));
+
+    return <ProjectsArchiveClient projects={protectedProjects} />;
 }
